22e7034fe8ff38877a3364b5fec3f28e4e456636
David Jandron <jandron1211@gmail.com>
2023-01-13 15:10:02 -0800

    wip bugfix branch selection continued

--- js/scripts.js
+++ js/scripts.js
@@ -7,29 +7,24 @@ function hideResults() {
 }
 
 //user interface logic
-window.onload = function() {
+window.onload = function handleRadio(event) {
   document.querySelector("form").onsubmit = function(event) {
     event.preventDefault();
     hideResults();
-    //const radio1 = document.querySelector("input#radio1").value;
-    //const radio2 = document.querySelector("input#radio2").value;
-    //const radio3 = document.querySelector("input#radio3").value;
     const operator = document.querySelector("input[name='operator']:checked").value;
     const operator2 = document.querySelector("input[name='operator2']:checked").value;
     const operator3 = document.querySelector("input[name='operator3']:checked").value;
-
     if (operator === "yes" && operator2 === "turtle" && operator3 === "dull") {
       document.getElementById("go").removeAttribute("class");
-    } else if (operator === "yes" && operator2 === "python" && operator3 === "sharp" || "dull") {
+    } else if (operator === "yes" && operator2 === "python" && operator3 === "") {
       document.getElementById("python").removeAttribute("class");
-    } else if (operator === "yes" && operator2 === "turtle" || "python" && operator3 === "sharp") {
+    } else if (operator === "yes" && operator2=== "python" || "turtle" && operator3 === "sharp") {
       document.getElementById("c").removeAttribute("class");
-    } else if (operator === "no" && operator2 === "turtle" || "python" && operator3 === "sharp" || "dull") {
+    } else if (operator === "no" && operator2 === "python" || "turtle" && operator3 === "sharp" || "dull") {
       document.getElementById("mc").removeAttribute("class");
     }
   }
 }
 window.addEventListener("load", function() {
-  const form = document.getElementById("language-selector");
-  form.addEventListener("submit", event);
+  document.getElementById("language-selector").addEventListener("submit", handleRadio);
 });
