b2ab9065e1f7e351ad94db9ffc6f367fcfaad87d
David Jandron <jandron1211@gmail.com>
2023-01-13 12:06:58 -0800

    wip java code to add index.html functionality

--- js/scripts.js
+++ js/scripts.js
@@ -5,17 +5,24 @@ function hideResults() {
   document.getElementById("c").setAttribute("class", "hidden");
 }
 
+//user interface logic
 window.onload = function() {
   document.querySelector("form").onsubmit = function(event) {
     event.preventDefault();
     hideResults();
-    const radio1 = document.querySelector("input#radio1").value;
-    const radio2 = document.querySelector("input#radio2").value;
-    const radio3 = document.querySelector("input#radio3").value;
+    //const radio1 = document.querySelector("input#radio1").value;
+    //const radio2 = document.querySelector("input#radio2").value;
+    //const radio3 = document.querySelector("input#radio3").value;
     const operator = document.querySelector("input[name='operator']:checked").value;
     const operator2 = document.querySelector("input[name='operator2']:checked").value;
     const operator3 = document.querySelector("input[name='operator3']:checked").value;
 
-    if (radio1)
+    if (operator === "yes" && operator2 === "snake" && operator3 === "sharp" || "dull") {
+      document.getElementById("python").removeAttribute("class");
+    } else if (operator === "yes" && operator2 === "snake" || "turtle" && operator3 === "sharp") {
+      document.getElementById("c").removeAttribute("class");
+    } else if (operator === "no" && operator2 === "snake" || "turtle" && operator3 === "sharp" || "dull") {
+      document.getElementById("go").removeAttribute("class");
+    }
   }
 }
